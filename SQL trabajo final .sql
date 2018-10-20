29b5dbd89e22c88898627746b39b6ae1d5ad685b
Gigni <german.niiya@gmail.com>
2018-10-20 20:42:49 -0300

    Version 1.1

--- SQL trabajo final .sql
+++ SQL trabajo final .sql
@@ -24,14 +24,14 @@ km_llegada INT not null,
 novedades VARCHAR(280),
 salida_id_recorrido INT not null,
 salida_combi_patente VARCHAR (8) not null,
-salida_chofer_dni INT not null,
-salida_id_planilla INT not null
+salida_chofer_dni INT not null
 );
 
-create table salida_pasajeros (
+/*create table salida_pasajeros (
 salida_id_salida INT not null,
 pasajero_dni INT not null
 );
+*/
 
 create table recorrido (
 id_recorrido INT primary key not null,
@@ -46,31 +46,35 @@ calle_Y VARCHAR(45) not null,
 parada_id_localidad INT not null
 );
 
-create table plan (
-id_plan INT primary key not null,
-abono_id_abono INT,
-recorrido_individual_id_recorrido_individual INT
+create table recorrido_paradas (
+recorrido_id_recorrido INT not null,
+parada_id_parada INT not null,
+primary key (recorrido_id_recorrido, parada_id_parada)
 );
 
-create table recorrido_paradas (
-id_recorrido_paradas INT primary key not null,
-recorrido_id_recorrido INT not null,
-parada_id_parada INT not null
+create table tarifa_plana_recorridos_paradas (
+tarifa_plana_id_tarifa_plana INT not null,
+recorrido_preferencial INT not null,
+parada_preferencial INT not null,
+primary key (tarifa_plana_id_tarifa_plana, recorrido_preferencial, parada_preferencial)
 );
 
 create table recorrido_individual (
 id_recorrido_individual INT primary key,
-monto_individual FLOAT not null,
-valido BOOLEAN,
-abordaje_id_recorrido_paradas INT not null
-);
-
-create table abono (
-id_abono INT primary key,
-monto_total FLOAT not null,
+monto FLOAT not null,
 fecha_desde DATE not null,
 fecha_hasta DATE not null,
-preferencial_id_parada INT
+pasajero_dni INT not null,
+recorrido_abordaje INT not null,
+parada_abordaje INT not null
+);
+
+create table tarifa_plana (
+id_tarifa_plana INT primary key,
+monto FLOAT not null,
+fecha_desde DATE not null,
+fecha_hasta DATE not null,
+pasajero_dni INT not null
 );
 
 create table domicilio (
@@ -105,18 +109,16 @@ create table planilla_chofer (
 pchofer_id_salida INT primary key not null,
 caja_inicial FLOAT not null,
 cantidad_boletos INT not null,
-monto_total FLOAT not null,
-caja_final FLOAT not null,
-gasto_inesperado FLOAT not null 
+monto_total FLOAT,
+caja_final FLOAT,
+gasto_inesperado FLOAT 
 );
-
 
 create table pasajero (
 dni INT primary key not null ,
 apellido VARCHAR(45) not null,
 nombre VARCHAR(45) not null,
-pasajero_id_domicilio INT not null,
-plan_activo_id_plan INT 
+pasajero_id_domicilio INT not null
 );
 
 create table mantenimiento (
@@ -152,16 +154,14 @@ razon_social VARCHAR(45) not null,
 cuit LONG not null,
 servicio_id_domicilio INT not null
 );
-/****************************************/
-/*agregar lista de pasajeros por parada*/
-/***************************************/
+
+
 
 /*----------------------------ASIGNACION DE FOREIGN KEYS------------------------------*/
 
 alter table salida add foreign key (salida_combi_patente) references combi(patente);
 alter table salida add foreign key (salida_chofer_dni) references chofer(dni);
 alter table salida add foreign key (salida_id_recorrido) references recorrido(id_recorrido);
-alter table salida add foreign key (salida_id_planilla) references planilla_chofer(pchofer_id_salida);
 
 alter table chofer add foreign key (chofer_id_domicilio) references domicilio(id_domicilio);
 
@@ -181,37 +181,43 @@ alter table  domicilio add foreign key (domicilio_id_localidad) references local
 alter table  estacion_de_servicio add foreign key (servicio_id_domicilio) references domicilio (id_domicilio);
 
 alter table  pasajero add foreign key (pasajero_id_domicilio) references domicilio(id_domicilio);
-alter table  pasajero add foreign key (plan_activo_id_plan) references plan(id_plan);
 
-alter table  plan add foreign key (abono_id_abono) references abono(id_abono);
-alter table  plan add foreign key (recorrido_individual_id_recorrido_individual) references recorrido_individual(id_recorrido_individual);
-
-alter table  recorrido_individual add foreign key (abordaje_id_recorrido_paradas) references recorrido_paradas(id_recorrido_paradas);
-
-alter table  abono add foreign key (preferencial_id_parada) references parada(id_parada);
+alter table  tarifa_plana add foreign key (pasajero_dni) references pasajero(dni);
 
 alter table  recorrido_paradas add foreign key (recorrido_id_recorrido) references recorrido(id_recorrido);
 alter table  recorrido_paradas add foreign key (parada_id_parada) references parada(id_parada);
 
-alter table salida_pasajeros add foreign key (salida_id_salida) references salida(id_salida);
-alter table salida_pasajeros add foreign key (pasajero_dni) references pasajero(dni);
+alter table  recorrido_individual add foreign key (pasajero_dni) references pasajero(dni);
+alter table  recorrido_individual add foreign key (recorrido_abordaje) references recorrido_paradas(recorrido_id_recorrido);
+alter table  recorrido_individual add foreign key (parada_abordaje) references recorrido_paradas(parada_id_parada);
 
+alter table tarifa_plana_recorridos_paradas add foreign key (tarifa_plana_id_tarifa_plana) references tarifa_plana(id_tarifa_plana);
+alter table tarifa_plana_recorridos_paradas add foreign key (recorrido_preferencial) references recorrido_paradas(recorrido_id_recorrido);
+alter table tarifa_plana_recorridos_paradas add foreign key (parada_preferencial) references recorrido_paradas(parada_id_parada);
+
+/*alter table salida_pasajeros add foreign key (salida_id_salida) references salida(id_salida);
+alter table salida_pasajeros add foreign key (pasajero_dni) references pasajero(dni);
+*/
+
+alter table planilla_chofer add foreign key (pchofer_id_salida) references salida(id_salida);
 
 
 /*------------------------------------DATOS-------------------------------------*/
-
 insert into combi values 
+-- Patente, modelo, marca, km_service
 ("AFJ579","Kombi T2", "Volkswagen",100),
 ("OP654UHG","Expert Tepee","Peugeot", 300),
 ("KJ579GHI","Trafic","Peugeot", 200),
 ("LK467HYG","Trafic","Peugeot", 200);
 
 insert into provincia values
+-- id_provincia, nombre_provincia
 (1,"Buenos Aires"),
 (2,"Santa Fe"),
 (3,"Corrientes");
 
 insert into localidad values
+-- id_localidad, nombre_localidad, id_provincia
 (1,"Lanús",1),
 (2,"Gerli",1),
 (3,"Alberdi",2),
@@ -223,6 +229,7 @@ insert into localidad values
 (9,"Agronomía",1);
 
 insert into domicilio values
+-- id_domicilio, calle, num_calle, id_localidad
 (1,"Salta",1264,1),
 (2,"Cangallo",954,2),
 (3,"Monteagudo",125,4),
@@ -243,20 +250,24 @@ insert into domicilio values
 (18,"Las Piedras",648,1);
 
 insert into chofer values
+-- dni, cuil, apellido, nombre, fecha_ingreso, n_licencia, id_domicilio
 (34668513,20346685138,"Dominguez","Juan",'2012-06-20',32157641,1),
 (38164236,20381642361,"Niiya","Germán",'2010-04-30',32414894,2),
 (36264876,20362648761,"Medina","Walter",'2016-12-06',24164876,5),
 (37498726,20374987267,"Noy","Martin",'2011-04-24',22679546,6);
 
 insert into taller values
+-- id_taller, razon_social, cuit, id_domicilio
 (1,"TecnoCar",24234569721,13),
 (2,"Enchulame La Combi",24674982631,11);
 
 insert into estacion_de_servicio values
+-- id_estacion, razon_social, cuit, id_domicilio
 (1,"Shell",32214795641,9),
 (2,"YPF",2349678762,14);
 
 insert into mantenimiento values
+-- id_mantenimiento, fecha_mantenimiento, descripcion, odometro, monto_service, id_taller, patente
 (1,'2018-06-13',"No importante",133,2000,1,"AFJ579"),
 (2,'2018-06-17',"",206,3100,1,"KJ579GHI"),
 (3,'2018-06-24',"Sigue sin importar",346,3000,2,"LK467HYG"),
@@ -269,6 +280,7 @@ insert into mantenimiento values
 (10,'2018-07-30',"No va a importar nunca",1235,3210,1,"KJ579GHI");
 
 insert into combustible values
+-- id_carga, fecha_carga, litros_cargados, montos_cargados, monto_carga, patente, id_estacion, chofer_dni
 (1,'2018-06-02',999,9999,"OP654UHG",1,34668513),
 (2,'2018-06-05',999,9999,"AFJ579",2,38164236),
 (3,'2018-06-11',999,9999,"LK467HYG",1,37498726),
@@ -281,24 +293,26 @@ insert into combustible values
 (10,'2018-07-27',999,9999,"KJ579GHI",1,37498726);
 
 insert into recorrido values
+-- id_recorrido, nombre_recorrido, distancia_recorrer
 (1,"Lanús-Constitución", 10),
 (2,"Lanús-Correo Central",13),
 (3,"Lanús-Agronomía",18);
 
 insert into salida values
-(1,999,'2018-06-03',6,999,"Ninguna",1,"KJ579GHI",38164236,1),
-(2,999,'2018-06-08',1,999,"Ninguna",3,"OP654UHG",34668513,5),
-(3,999,'2018-06-14',5,999,"Ninguna",2,"AFJ579",37498726,6),
-(4,999,'2018-06-25',4,999,"Ninguna",1,"LK467HYG",34668513,7),
-(5,999,'2018-06-30',6,999,"Ninguna",1,"OP654UHG",38164236,10),
-(6,999,'2018-07-02',7,999,"Ninguna",2,"KJ579GHI",37498726,9),
-(7,999,'2018-07-12',8,999,"Ninguna",2,"LK467HYG",36264876,8),
-(8,999,'2018-07-15',4,999,"Ninguna",3,"AFJ579",34668513,4),
-(9,999,'2018-07-20',5,999,"Ninguna",3,"KJ579GHI",38164236,2),
-(10,999,'2018-07-24',2,999,"Ninguna",2,"OP654UHG",36264876,3);
-
+-- id_salida, km_salida, fecha, pasajes_vendidos, km_llegada, novedades, id_recorrido, patente, chofer_dni
+(1,999,'2018-06-03',6,999,"Ninguna",1,"KJ579GHI",38164236),
+(2,999,'2018-06-08',1,999,"Ninguna",3,"OP654UHG",34668513),
+(3,999,'2018-06-14',5,999,"Ninguna",2,"AFJ579",37498726),
+(4,999,'2018-06-25',4,999,"Ninguna",1,"LK467HYG",34668513),
+(5,999,'2018-06-30',6,999,"Ninguna",1,"OP654UHG",38164236),
+(6,999,'2018-07-02',7,999,"Ninguna",2,"KJ579GHI",37498726),
+(7,999,'2018-07-12',8,999,"Ninguna",2,"LK467HYG",36264876),
+(8,999,'2018-07-15',4,999,"Ninguna",3,"AFJ579",34668513),
+(9,999,'2018-07-20',5,999,"Ninguna",3,"KJ579GHI",38164236),
+(10,999,'2018-07-24',2,999,"Ninguna",2,"OP654UHG",36264876);
 
 insert into parada values
+-- id_parada, calle_x, calle_y, id_localidad
 (1,"Av Yrigoyen","Quintana",1),
 (2,"Av Eva Peron","Anatole France",1),
 (3,"Bustamante","Padilla",2),
@@ -325,68 +339,84 @@ insert into parada values
 (24,"Pringles","Damonte",1);*/
 
 insert into recorrido_paradas values
-(1,1,1),
-(2,1,2),
-(3,1,3),
-(4,1,4),
-(5,1,5),
-(6,1,6),
-(7,1,7),
-(8,1,10),
-(9,2,1),
-(10,2,8),
-(11,2,9),
-(12,2,11),
-(13,2,12),
-(14,2,13),
-(15,2,10),
-(16,2,21),
-(17,3,1),
-(18,3,14),
-(19,3,15),
-(20,3,16),
-(21,3,17),
-(22,3,18),
-(23,3,10),
-(24,3,21);
-
-insert into abono values
-(1,200,'2018-05-21','2018-06-20',1),
-(2,300,'2018-06-21','2018-07-20',13);
-
-insert into recorrido_individual values
-(1,15,false,1),
-(2,15,false,1),
-(3,15,false,10),
-(4,16,false,14),
-(5,16,true,12),
-(6,16,true,11),
-(7,16,true,5);
-
-insert into plan values
-(1, 1, null), -- ID plan, abono, viaje individual.
-(2, 2, null),
-(3, null, 1),
-(4, null, 2),
-(5, null, 3),
-(6, null, 4),
-(7, null, 5),
-(8, null, 6),
-(9, null, 7);
+-- id_recorrido, id_parada
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(1,5),
+(1,6),
+(1,7),
+(1,10),
+(2,1),
+(2,8),
+(2,9),
+(2,11),
+(2,12),
+(2,13),
+(2,10),
+(2,21),
+(3,1),
+(3,14),
+(3,15),
+(3,16),
+(3,17),
+(3,18),
+(3,10),
+(3,21);
 
 insert into pasajero values
-(25456825,"Rodriguez","Pablo",3,1),
-(34567882,"Barroso","Daniel",4,2),
-(23417982,"Vila","Maria",7,3),
-(35762199,"Rio","Carmen",8,4),
-(40241967,"Duran","Luis",10,5),
-(41216748,"Cortes","Ignacio",12,6),
-(39413579,"Sánchez","Pilar",15,7),
-(38541967,"Daniel","Martin",16,8),
-(25216796,"Rubio","María",17,9),
-(36449792,"Roncero","Alejandra",18,null);
+-- dni, apellido, nombre, id_domicilio
+(25456825,"Rodriguez","Pablo",3),
+(34567882,"Barroso","Daniel",4),
+(23417982,"Vila","Maria",7),
+(35762199,"Rio","Carmen",8),
+(40241967,"Duran","Luis",10),
+(41216748,"Cortes","Ignacio",12),
+(39413579,"Sánchez","Pilar",15),
+(38541967,"Daniel","Martin",16),
+(25216796,"Rubio","María",17),
+(36449792,"Roncero","Alejandra",18);
 
-insert into salida_pasajeros values
+insert into tarifa_plana values
+-- id_tarifa_plana, monto, fecha_desde, fecha_hasta, pasajero_dni
+(1,200,'2018-05-21','2018-06-20',38541967),
+(2,300,'2018-06-21','2018-07-20',25216796);
+
+insert into recorrido_individual values
+-- id_recorrido_individual, monto, fecha_desde, fecha_hasta, pasajero_dni, recorrido_abordaje, parada_abordaje
+(1,200,'2018-04-10','2018-05-10',25456825,2,9),
+(2,200,'2018-05-15','2018-06-15',34567882,3,14),
+(3,200,'2018-06-15','2018-07-15',23417982,1,1),
+(4,200,'2018-05-20','2018-06-20',35762199,2,1),
+(5,200,'2018-07-23','2018-08-23',40241967,3,1),
+(6,200,'2018-08-14','2018-09-14',41216748,2,12),
+(7,200,'2018-05-1','2018-06-1',39413579,1,3);
+
+insert into planilla_chofer values
+-- id_salida, caja_inicial, cantidad_boletos, monto_total, caja_final, gasto_inesperado
+(1,3000,6,9999,9999,0),
+(2,3000,1,9999,9999,0),
+(3,3000,5,9999,9999,0),
+(4,3000,4,9999,9999,0),
+(5,3000,5,9999,9999,0),
+(6,3000,5,9999,9999,0),
+(7,3000,7,9999,9999,0),
+(8,3000,4,9999,9999,0),
+(9,3000,5,9999,9999,0),
+(10,3000,2,9999,9999,0);
+
+insert into tarifa_plana_recorridos_paradas values
+-- id_tarifa_plana, recorrido_preferencial, parada_preferencial
+(1,1,4),
+(1,2,8),
+(1,3,16),
+(2,1,1),
+(2,2,1),
+(2,3,14);
+
+
+/*insert into salida_pasajeros values
 (1,23417982),
 (1,35762199),
 (1,40241967),
@@ -407,23 +437,17 @@ insert into salida_pasajeros values
 (9,23417982),
 (9,38541967),
 (10,36449792);
-
-insert into planilla_chofer values
-(1,300,20,3000,2500,500),
-(2,300,25,3250,3100,150),
-(3,300,27,3400,2900,500),
-(4,300,17,2800,2700,100),
-(5,300,22,3080,3080,0),
-(6,300,30,3600,3400,200),
-(7,300,19,2920,2700,220),
-(8,300,20,3000,2900,100),
-(9,300,22,3080,2900,180),
-(10,300,25,3250,3000,250);
-
+*/
 
 
 /*------------------------------------CONSULTAS-------------------------------------*/
-select* from pasajero;
+-- 1) Emitir listados de clientes, con su correspondiente abono si lo tiene.
+/*
+select apellido, nombre, abono_id_abono 
+from pasajero
+inner join plan
+on plan_activo_id_plan=id_plan;
+
 -- 2) Emitir listado de recorridos, con sus paradas.
 select nombre_recorrido, calle_x, calle_y
 from recorrido
@@ -441,31 +465,24 @@ on combi.patente=mantenimiento.mantenimiento_combi_patente;
 
 -- 4) Confeccionar las planillas de salida, con las paradas en las que deba detenerse el móvil, y los pasajeros a recoger en cada una de ellas.
 
+select fecha, dni, abordaje_id_recorrido_paradas
+from salida
+inner join salida_pasajeros
+on salida.id_salida=salida_pasajeros.salida_id_salida
+inner join pasajero
+on salida_pasajeros.pasajero_dni=pasajero.dni
+inner join plan
+on pasajero.plan_activo_id_plan=plan.id_plan
+inner join recorrido_individual
+on plan.recorrido_individual_id_recorrido_individual=recorrido_individual.id_recorrido_individual;
+
 -- 5) Consultar la recaudación bruta ya sea por recorrido o por destino (provincia de Buenos Aires o CABA)  
 -- 6) Consultar los gastos por recorrido, por chofer o por chofer y entre fechas.
-select s.fecha as fecha,sum(pc.gasto_inesperado) as monto, c.nombre as chofer, r.nombre_recorrido as recorrido
-from salida s
-inner join planilla_chofer pc on pc.pchofer_id_salida=s.salida_id_planilla
-inner join chofer c on c.dni=s.salida_chofer_dni
-inner join recorrido r on r.id_recorrido=s.salida_id_recorrido
-group by chofer,recorrido;
 
 -- 7) Emitir un listado mensual de los gastos realizados por todos los choferes. 
-select c.nombre as chofer, sum(pc.gasto_inesperado) as total_gastado
-from salida s
-inner join chofer c on c.dni=s.salida_chofer_dni
-inner join planilla_chofer pc on pc.pchofer_id_salida=s.salida_id_planilla
-where fecha between '20180601' AND '20180731'
-group by chofer;
 -- 8) Emitir listados de consumo medio de combustible por km entre fechas (por recorrido, por móvil o por chofer), ordenado de mayor a menor. 
 -- 9) Emitir listados de ganancia bruta por km, por recorrido 
--- 10) Emitir listado de cantidad de pasajeros transportados entre fechas por recorrido.
-select r.nombre_recorrido as recorrido, sum(s.pasajes_vendidos) as totalxrecorrido
-from salida s
-inner join recorrido r on r.id_recorrido=s.salida_id_recorrido
-where fecha between '20180601' AND '20180731'
-group by recorrido;
-
+-- 10) Emitir listado de cantidad de pasajeros transportados entre fechas por recorrido. 
 -- 11)Emitir listado mensual de recaudación por recorrido o parada final. 
 -- 12)Emitir listado de km recorridos entre fecha, para móvil o chofer.
 
@@ -485,3 +502,13 @@ group by patente;
 
 -- 14)Emitir listado de ganancia bruta por recorrido, calculada como el total de pasajes vendidos menos los gastos entre fechas. 
 -- 15)Consultar la ganancia bruta de la empresa, calculada como el total de pasajes vendidos, más los abonos, menos el combustible y menos los gastos de mantenimiento. 
+
+select * from pasajero pa
+inner join plan pl
+on pa.plan_activo_id_plan=pl.id_plan
+inner join recorrido_individual ri
+on pl.recorrido_individual_id_recorrido_individual=ri.id_recorrido_individual
+inner join abono a
+on pl.abono_id_abono=a.id_abono
+order by apellido DESC;
+
